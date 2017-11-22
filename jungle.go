498dbda15914abd6824a7652dcc6197f6dff7840
caimmy <caimmy@qq.com>
2017-11-22 12:01:00 +0800

    t

--- jungle.go
+++ jungle.go
@@ -1,1 +1,20 @@
 package jungle
+
+import (
+	"net/http"
+	"io"
+	"log"
+)
+
+func indexPage(w http.ResponseWriter, r http.Request) {
+	io.WriteString(w, "hello jungle!")
+}
+
+func run() {
+
+	http.HandleFunc("/", indexPage)
+	err := http.ListenAndServe("8080", nil)
+	if (err != nil) {
+		log.Fatal("ListenAndServ: ", err.Error())
+	}
+}
